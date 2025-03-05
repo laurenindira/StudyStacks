d280d5eba8cfd6af3f9b29c6362b7630a4c504b0
bkatler <112525119+bkatler@users.noreply.github.com>
2025-03-05 12:39:53 -0800

    Adding Profile Page docs

---
+++ StudyStacksProfilePage/ProfileApp/ProfileApp/User.swift
@@ -0,0 +1,17 @@
+//
+//  User.swift
+//  ProfileApp
+//
+//  Created by brady katler on 3/3/25.
+//
+
+import Foundation
+
+struct User {
+    var name: String
+    var username: String
+    var memberSince: String
+    var streak: Int
+    var stacks: Int
+    var badges: Int
+}
---
+++ StudyStacksProfilePage/ProfileApp/ProfileAppTests/ProfileAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileAppTests.swift
+//  ProfileAppTests
+//
+//  Created by brady katler on 3/3/25.
+//
+
+import Testing
+@testable import ProfileApp
+
+struct ProfileAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
