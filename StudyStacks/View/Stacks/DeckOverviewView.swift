0a0a255f9876e59770346b7f5c5643ce0191e23e
giselleeliasi <112521190+giselleeliasi@users.noreply.github.com>
2025-04-22 12:19:46 -0700

    flip is better

--- StudyStacks/View/Stacks/DeckOverviewView.swift
+++ StudyStacks/View/Stacks/DeckOverviewView.swift
@@ -47,36 +47,37 @@ struct StackDetailView: View {
                     .padding(.horizontal)
 
                     ZStack {
-                        RoundedRectangle(cornerRadius: 12)
-                            .fill(Color.gray.opacity(0.2))
-                            .frame(width: 340, height: 200)
-                            .overlay(
-                                ZStack {
-                                    if !isFlipped {
-                                        Text(stack.cards[currentCardIndex].front)
-                                            .font(.title2)
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.black)
-                                            .rotation3DEffect(.degrees(isFlipped ? 180 : 0), axis: (x: 1, y: 0, z: 0))
-                                            .opacity(isFlipped ? 0 : 1)
-                                    }
-                                    if isFlipped {
-                                        Text(stack.cards[currentCardIndex].back)
-                                            .font(.title2)
-                                            .fontWeight(.bold)
-                                            .foregroundColor(.black)
-                                            .rotation3DEffect(.degrees(isFlipped ? 180 : 0), axis: (x: 1, y: 0, z: 0))
-                                            .opacity(isFlipped ? 1 : 0)
-                                    }
-                                }
-                            )
-                            .rotation3DEffect(.degrees(isFlipped ? 180 : 0), axis: (x: 1, y: 0, z: 0))
-                            .animation(.easeInOut(duration: 0.6), value: isFlipped)
-                            .onTapGesture {
-                                withAnimation {
-                                    isFlipped.toggle()
-                                }
-                            }
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 12)
+                                .fill(Color.gray.opacity(0.2))
+                                .frame(width: 340, height: 200)
+                                .overlay(
+                                    Text(stack.cards[currentCardIndex].front)
+                                        .font(.title2)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.black)
+                                        .padding()
+                                )
+                                .opacity(isFlipped ? 0 : 1)
+                                .rotation3DEffect(.degrees(isFlipped ? 180 : 0), axis: (x: 0, y: 1, z: 0))
+                            
+                            RoundedRectangle(cornerRadius: 12)
+                                .fill(Color.gray.opacity(0.2))
+                                .frame(width: 340, height: 200)
+                                .overlay(
+                                    Text(stack.cards[currentCardIndex].back)
+                                        .font(.title2)
+                                        .fontWeight(.bold)
+                                        .foregroundColor(.black)
+                                        .padding()
+                                )
+                                .opacity(isFlipped ? 1 : 0)
+                                .rotation3DEffect(.degrees(isFlipped ? 0 : -180), axis: (x: 0, y: 1, z: 0))
+                        }
+                        .animation(.easeInOut(duration: 0.5), value: isFlipped)
+                        .onTapGesture {
+                            isFlipped.toggle()
+                        }
                         
                         HStack {
                             Button(action: {
